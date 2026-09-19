01d22d4fa7fc876d1517b665548600895f1eb157
Arjun <144340917+arjuunns@users.noreply.github.com>
2026-09-19 07:06:18 +0530

    Add: sql-highest-grade-for-each-student - submission-0

---
+++ SQL/sql-highest-grade-for-each-student/submission-0.sql
@@ -0,0 +1,15 @@
+WITH cte AS (
+SELECT
+student_id,
+exam_id,
+score,
+ROW_NUMBER() OVER (PARTITION BY student_id ORDER BY score DESC, exam_id ASC) as rn
+FROM exam_results
+)
+SELECT
+student_id,
+exam_id,
+score
+FROM cte
+WHERE rn = 1
+ORDER BY student_id
